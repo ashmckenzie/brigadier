157738905653f745b655a4ea012368cdf5079aa0
Ash McKenzie <ash@the-rebellion.net>
2016-02-12 14:59:24 +1000

    Added rubocop and simplecov

--- spec/brigadier_spec.rb
+++ spec/brigadier_spec.rb
@@ -4,8 +4,4 @@ describe Brigadier do
   it 'has a version number' do
     expect(Brigadier::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
